d873ebdb5cf34e0e7e62d5a71d2c6498d911037d
leriil <leriil@mail.ru>
2018-10-20 21:49:14 +0300

    Removed all unused methods

--- src/main/java/com/tsystems/tshop/repositories/AddressRepository.java
+++ src/main/java/com/tsystems/tshop/repositories/AddressRepository.java
@@ -8,5 +8,10 @@ import java.util.List;
 
 @Repository
 public interface AddressRepository extends JpaRepository<Address,Long> {
-    public List<Address> findByUsers_Login(String login);
+//    public List<Address> findByUsers_Login(String login);
+
+    public List<Address> findByCountry(String country);
+
+//    @Query("select a from Address a JOIN a.users u where u.login=:login" )
+//    public List<Address> queryOne(String login);
 }
--- src/main/java/com/tsystems/tshop/repositories/UserRepository.java
+++ src/main/java/com/tsystems/tshop/repositories/UserRepository.java
@@ -6,4 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface UserRepository extends JpaRepository<User,Long> {
     public User findByLogin(String login);
 
+
+//    @Query("select u from User u join u.addresses a where u.login=:login ")
+////    public User queryTwo(@Param ("login") String login);
 }
