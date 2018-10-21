aca23ce7ea9a00c0b67454badde5d90b58cbe07b
leriil <leriil@mail.ru>
2018-10-21 19:17:56 +0300

    method for repeating the order is added

--- src/main/java/com/tsystems/tshop/controllers/SaleController.java
+++ src/main/java/com/tsystems/tshop/controllers/SaleController.java
@@ -11,12 +11,13 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
 
 import java.util.logging.Logger;
 @Controller
 @RequestMapping("/order")
-//@SessionAttributes({"order","product","products","productCounter"})
+@SessionAttributes({"order"})
 
 public class SaleController {
 
@@ -31,27 +32,36 @@ public class SaleController {
 @Autowired
     ProductService productService;
 
+@ModelAttribute("order")
+public Sale getSale(){
+    return new Sale();
+}
+
 @RequestMapping("/history")
 public String repeatOrder(Model model){
     String login=SecurityContextHolder.getContext().getAuthentication().getName();
     model.addAttribute("orders",this.saleService.findUserOrders(login));
+    System.out.println("from /history   "+this.saleService.findUserOrders(login));
 
     return "order_history";
 }
 
     @RequestMapping(value = "/{saleId}")
-    public String findProduct(Model model, @PathVariable Long saleId){
+    public String findProduct(@ModelAttribute("order") Sale sale, Model model, @PathVariable Long saleId){
         try{model.addAttribute("order",this.saleService.findSale(saleId));
+            System.out.println("from /{saleId} "+this.saleService.findSale(saleId));
         }catch (RuntimeException e){
             return "order_not_found";
         }
         return "order";
     }
 
-    @RequestMapping(value = "/order/save")
+    @RequestMapping(value = "/repeat/save")
     public String saveSale(@ModelAttribute("order")Sale sale, SessionStatus status){
+    sale.setSaleId(null);
+        System.out.println("from /repeat/save   "+sale);
         this.saleService.saveSale(sale);
-        System.out.println(sale);
+        System.out.println("after saving a sale "+sale);
         status.setComplete();
         return "redirect:/product/all";
     }
