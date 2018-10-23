2ac100612c27157344961985b76464889f5832f5
leriil <leriil@mail.ru>
2018-10-23 12:38:18 +0300

    added getUser() - no args

--- src/main/java/com/tsystems/tshop/services/UserService.java
+++ src/main/java/com/tsystems/tshop/services/UserService.java
@@ -3,6 +3,7 @@ package com.tsystems.tshop.services;
 import com.tsystems.tshop.domain.User;
 import com.tsystems.tshop.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,9 +12,14 @@ public class UserService {
     @Autowired
     public UserRepository userRepository;
 
-    public User getUser(String login){
+
+    public User getUser(){
+        String login=SecurityContextHolder.getContext().getAuthentication().getName();
         return this.userRepository.findByLogin(login);
     }
+public User getUser(String login){
+        return this.userRepository.findByLogin(login);
+}
 
 
 }
