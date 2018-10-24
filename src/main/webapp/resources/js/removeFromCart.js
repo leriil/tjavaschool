b2c567a43650d353426f5cf10674618f7d41fe3b
leriil <leriil@mail.ru>
2018-10-25 00:51:19 +0300

    added function for removing products from cart

---
+++ src/main/webapp/resources/js/removeFromCart.js
@@ -0,0 +1,23 @@
+// $(document).ready(removeProduct);
+
+function removeProduct (id) {
+    var csrfParameter = $("meta[name='_csrf_parameter']").attr("content");
+    var csrfHeader = $("meta[name='_csrf_header']").attr("content");
+    var csrfToken = $("meta[name='_csrf']").attr("content");
+    var headers = {};
+    headers[csrfHeader] = csrfToken;
+    $.ajax({
+            url: ctx+"/order/cart/remove",
+            dataType: 'json',
+            type:'post',
+            headers:headers,
+            contentType:'application/json',
+            data:JSON.stringify(Number(id)),
+            processData:false
+        }
+    )
+        // .done(function (result) { $("#basket").html(result); })
+        .fail(function () { alert("the product wasn't removed from your cart"); });
+}
+
+
