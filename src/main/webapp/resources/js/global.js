05d0fdc22e7d98e05b1b11c1aead8070f555f3d3
leriil <leriil@mail.ru>
2018-10-25 00:52:55 +0300

    $ajax for getting the number of products in cart added

--- src/main/webapp/resources/js/global.js
+++ src/main/webapp/resources/js/global.js
@@ -1,7 +1,20 @@
 $(document).ready(function () {
+    var products=0;
     $("#logout").click(function (e) {
         e.preventDefault();
         $("#logout-form").submit();
     })
+    $.ajax({
+            url: ctx+"/order/cart/count",
+            dataType: 'json',
+            type:'get',
+            // headers:headers,
+            contentType:'application/json',
+            // data:JSON.stringify(Number(id)),
+            processData:false
+        }
+    )
+        .done(function (result) { $("#basket").html(result); })
+        .fail(function () { alert("the product wasn't removed from your cart"); });
     }
 );
