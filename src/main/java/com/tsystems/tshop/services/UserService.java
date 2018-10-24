bca0d066f623e7400663c2f1fa23e8adb0d3fc80
leriil <leriil@mail.ru>
2018-10-25 01:05:01 +0300

    saveAndAuthenticateNewUser added

--- src/main/java/com/tsystems/tshop/services/UserService.java
+++ src/main/java/com/tsystems/tshop/services/UserService.java
@@ -1,16 +1,41 @@
 package com.tsystems.tshop.services;
 
+import com.tsystems.tshop.domain.Role;
 import com.tsystems.tshop.domain.User;
+import com.tsystems.tshop.repositories.RoleRepository;
 import com.tsystems.tshop.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Service
 public class UserService {
 
     @Autowired
     public UserRepository userRepository;
+
+    @Autowired
+    RoleRepository roleRepository;
+
+    @Autowired
+    RoleService roleService;
+
+    public void saveAndAuthenticateNewUser(User user){
+        Set<Role> roles=new HashSet<>();
+        roles.add(this.roleService.getRoleByName("CLIENT"));
+        user.setRoles(roles);
+        this.userRepository.save(user);
+        //build an authentication object for the user
+        Authentication authentication=new UsernamePasswordAuthenticationToken(user, user.getPassword(),user.getAuthorities());
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+    }
+
+
 
 
     public User getUser(){
