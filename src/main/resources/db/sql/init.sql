7a9491412c2721fdc2fcf26c845915c812feeb5f
leriil <leriil@mail.ru>
2018-10-25 01:22:45 +0300

    more users added, dates of birth corrected

--- src/main/resources/db/sql/init.sql
+++ src/main/resources/db/sql/init.sql
@@ -1,10 +1,14 @@
-insert into role(name) values ('client');
-insert into role(name) values ('salesperson');
+insert into role(name) values ('CLIENT');
+insert into role(name) values ('SALESPERSON');
 
-insert into user(name, surname, login, password, birth_date, email) values ('Bill','Murrey','bill','$2a$04$dBOzyg8ikiHaKWJJONbgquqb9.gK4w5mOhAEAJcovHFTGRTxqBhTO' ,'2000-02-12','bill@yandex.ru');
-insert into user(name, surname, login, password, birth_date, email) values ('Bob','Marley','bob','$2a$04$Tv6i5jzZ7OzQ50gMm9FhC.CmoOZh1OtSZlbktGSz31FEjV2oDu8Im' ,'1990-04-15','bob@yandex.ru');
-insert into user(name, surname, login, password, birth_date, email) values ('Mary','Fisher','marry','$2a$04$LGSvlBTOvbQsdEszhhZItu4NToZQT3AzTH82d6vwEaPrXeyLmGaZW' ,'1988-10-05','mary@yandex.ru');
-insert into user(name, surname, login, password, birth_date, email) values ('Susan','Mayer','susy','$2a$04$Hz5QllwBVlxte3OeqHfVl.anGjMtMt2PBVFCf8tS2M6xTFVB4Gqji' ,'2001-04-18','suzy@yandex.ru');
+insert into user(name, surname, login, password, birth_date, email) values ('Bill','Murrey','bill','b' ,'2000-01-12','bill@yandex.ru');
+insert into user(name, surname, login, password, birth_date, email) values ('Paul','Marley','paul','p' ,'1968-04-03','bob@yandex.ru');
+insert into user(name, surname, login, password, birth_date, email) values ('Mary','Fisher','marry','m' ,'1856-06-20','mary@yandex.ru');
+insert into user(name, surname, login, password, birth_date, email) values ('Susan','Mayer','susy','s' ,'2002-12-29','suzy@yandex.ru');
+-- insert into user(name, surname, login, password, birth_date, email) values ('Bill','Murrey','bill','$2a$04$dBOzyg8ikiHaKWJJONbgquqb9.gK4w5mOhAEAJcovHFTGRTxqBhTO' ,'2000-01-12','bill@yandex.ru');
+-- insert into user(name, surname, login, password, birth_date, email) values ('Bob','Marley','bob','$2a$04$Tv6i5jzZ7OzQ50gMm9FhC.CmoOZh1OtSZlbktGSz31FEjV2oDu8Im' ,'04-10-1968','bob@yandex.ru');
+-- insert into user(name, surname, login, password, birth_date, email) values ('Mary','Fisher','marry','$2a$04$LGSvlBTOvbQsdEszhhZItu4NToZQT3AzTH82d6vwEaPrXeyLmGaZW' ,'10-05-1856','mary@yandex.ru');
+-- insert into user(name, surname, login, password, birth_date, email) values ('Susan','Mayer','susy','$2a$04$Hz5QllwBVlxte3OeqHfVl.anGjMtMt2PBVFCf8tS2M6xTFVB4Gqji' ,'04-01-2002','suzy@yandex.ru');
 
 insert into product(name, category, in_stock, price, volume, weight) values ('laptop','DEVICE',20,1000.00 ,NULL ,500);
 insert into product(name, category, in_stock, price, volume, weight) values ('iphone','DEVICE',10,400.00 ,NULL ,100);
