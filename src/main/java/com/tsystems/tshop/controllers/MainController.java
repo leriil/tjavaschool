498ab532a5831a5b7100fc91b660023d8b5d5078
leriil <leriil@mail.ru>
2018-10-24 20:04:17 +0300

    methods for registering new users added

--- src/main/java/com/tsystems/tshop/controllers/MainController.java
+++ src/main/java/com/tsystems/tshop/controllers/MainController.java
@@ -1,7 +1,11 @@
 package com.tsystems.tshop.controllers;
 
+import com.tsystems.tshop.domain.User;
+import com.tsystems.tshop.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -16,6 +20,23 @@ public class MainController {
 
     private static final Logger log=Logger.getLogger("Log");
 
+    @Autowired
+    UserService userService;
+
+
+
+
+    @RequestMapping(value="/register",method = RequestMethod.POST)
+    public String register(@ModelAttribute User user){
+        this.userService.saveAndAuthenticateNewUser(user);
+        return "register";
+    }
+
+
+    @RequestMapping(value="/register",method = RequestMethod.GET)
+    public String goRegister(){
+        return "register";
+    }
     @RequestMapping(method = RequestMethod.GET)
     public String start(Model model){
         return "redirect:/product/all";
