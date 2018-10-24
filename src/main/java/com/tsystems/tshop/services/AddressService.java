658bac8f4135aacf49c513598b2e99f86ea22a6b
leriil <leriil@mail.ru>
2018-10-25 01:20:52 +0300

    userCurrentAddress added

--- src/main/java/com/tsystems/tshop/services/AddressService.java
+++ src/main/java/com/tsystems/tshop/services/AddressService.java
@@ -1,13 +1,12 @@
 package com.tsystems.tshop.services;
 
 import com.tsystems.tshop.domain.Address;
+import com.tsystems.tshop.domain.User;
 import com.tsystems.tshop.repositories.AddressRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
 @Service
@@ -18,19 +17,30 @@ public class AddressService {
     @Autowired
     AddressRepository addressRepository;
 
-    public void saveAddress(Address address){
-        List<Address>addresses=new ArrayList<>();
-        addresses=this.addressRepository.findAll();
-        for (int i = 0; i <addresses.size() ; i++) {
-            if (addresses.get(i).equals(address)){
-                log.log(Level.WARNING,"this address: "+address.toString()+" is already in the database");
-            }else {this.addressRepository.save(address);}
-        }
+    @Transactional
+public Address userCurrentAddress(User user){
+    if(user.getAddresses().iterator().hasNext()){
+        return user.getAddresses().iterator().next();
+    }else return null;
+}
+
+    public void saveAddress(Address oldAddress, Address newAddress) {
+
     }
+}
+//        while (iterator.hasNext()) {
+//            Address a = iterator.next();
+//
+//            if (address.equals(a)) {
+//                log.log(Level.INFO, "this address: " + address.toString() + " is already in the database");
+//                break;
+//            } else this.addressRepository.save(address);
+//        }
 
 //    public List<Address> getUserAddresses(String login){
 //        return this.addressRepository.findByUsers_Login(login);
 //    }
 
 
-}
+
+
