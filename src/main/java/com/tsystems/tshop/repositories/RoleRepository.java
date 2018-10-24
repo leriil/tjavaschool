ca4b0eb46ecc7c0b40a397f0b34decd7bd2c3101
leriil <leriil@mail.ru>
2018-10-25 00:31:14 +0300

    getByName added

---
+++ src/main/java/com/tsystems/tshop/repositories/RoleRepository.java
@@ -0,0 +1,8 @@
+package com.tsystems.tshop.repositories;
+
+import com.tsystems.tshop.domain.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role,Long> {
+    public Role getByName(String name);
+}
