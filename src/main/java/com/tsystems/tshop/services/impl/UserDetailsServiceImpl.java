5d3bc683c9efc20e0ff8ee7cd2cd846520c39ebc
leriil <leriil@mail.ru>
2018-10-25 01:19:33 +0300

    returns User

--- src/main/java/com/tsystems/tshop/services/impl/UserDetailsServiceImpl.java
+++ src/main/java/com/tsystems/tshop/services/impl/UserDetailsServiceImpl.java
@@ -1,18 +1,11 @@
 package com.tsystems.tshop.services.impl;
 
-import com.tsystems.tshop.domain.Role;
-import com.tsystems.tshop.domain.User;
 import com.tsystems.tshop.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
 
 @Component("userDetailsServiceImpl")
 public class UserDetailsServiceImpl implements UserDetailsService {
@@ -20,24 +13,8 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     public UserService userService;
     @Override
     public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
-        // с помощью нашего сервиса UserService получаем User
-        User user = userService.getUser(s);
-        // указываем роли для этого пользователя
-//        Set<GrantedAuthority> roles = new HashSet();
-//        roles.add(new SimpleGrantedAuthority(UserRole.USER.name()));
 
-        Set<Role> roles=user.getRoles();
-        List<String>privileges=new ArrayList<>();
-        for (Role role:roles) {
-            privileges.add(role.getName());
-            System.out.println(privileges);
-        }
+        return userService.getUser(s);
 
-        // на основании полученных данных формируем объект UserDetails
-        // который позволит проверить введенный пользователем логин и пароль
-        // и уже потом аутентифицировать пользователя
-
-                return new org.springframework.security.core.userdetails.User(user.getLogin(),
-                        user.getPassword(),AuthorityUtils.createAuthorityList(String.valueOf(privileges)));
     }
 }
