9eddf71a2a9712c5df2d5e6ec8635dd01b2adeb1
leriil <leriil@mail.ru>
2018-10-28 03:29:58 +0300

    added a namedNativeQuery to get 2 top products

--- src/main/java/com/tsystems/tshop/services/ProductService.java
+++ src/main/java/com/tsystems/tshop/services/ProductService.java
@@ -5,6 +5,9 @@ import com.tsystems.tshop.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
 import java.util.logging.Level;
@@ -16,6 +19,9 @@ public class ProductService {
     private static final java.util.logging.Logger log = Logger.getLogger("Log");
     @Autowired
     ProductRepository repository;
+
+    @PersistenceContext
+    EntityManager em;
 
     public void save(Product product) {
         this.repository.save(product);
@@ -34,7 +40,19 @@ public class ProductService {
             throw new RuntimeException();}
     }
 
+    @Transactional
     public List<Product> getTopProducts(){
-        return this.repository.getTop();
+
+        javax.persistence.Query query=em.createNativeQuery("select product_id, category, in_stock, name, price, volume, weight " +
+                "from(select * from product left join " +
+                "(select product.product_id as pid, count(sale_product.product_id) as top " +
+                "from product join sale_product " +
+                "on product.product_id=sale_product.product_id " +
+                "group by pid) as s on product.product_id=s.pid) as p order by p.top desc");
+
+        List<Product> topProducts=query.setMaxResults(2).getResultList();
+
+        return topProducts;
     }
+
 }
