62bb679b50f346759f1655a65ec04fd902f106ab
leriil <leriil@mail.ru>
2018-11-03 22:29:51 +0300

    suppressWarnings added, return without an intermediate variable "topProducts"

--- src/main/java/com/tsystems/tshop/repositories/ProductRepositoryImpl.java
+++ src/main/java/com/tsystems/tshop/repositories/ProductRepositoryImpl.java
@@ -13,19 +13,18 @@ public class ProductRepositoryImpl implements ProductRepositoryCustom {
     EntityManager em;
 
     @Override
+    @SuppressWarnings("unchecked")
     public List<ProductTop> getTopProducts() {
 
-        List<ProductTop> topProducts=
+        return
                 em.createNativeQuery("select product_id as productId, name, category, in_stock as inStock, price " +
                 "from(select * from product left join " +
-                "(select product.product_id as pid, count(sale_product.product_id) as top " +
-                "from product join sale_product " +
-                "on product.product_id=sale_product.product_id " +
+                "(select product.product_id as pid, count(order_product.product_id) as top " +
+                "from product join order_product " +
+                "on product.product_id=order_product.product_id " +
                 "group by pid) as s on product.product_id=s.pid) as p order by p.top desc",
                         "productTopMapping")
                         .setMaxResults(2)
                         .getResultList();
-
-        return topProducts;
     }
 }
