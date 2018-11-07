c1cfb2e96697022e467437dd119cd4290cc7c5c8
leriil <leriil@mail.ru>
2018-11-07 18:52:54 +0300

    added function which changes the image of the sort button

--- src/main/webapp/resources/js/global.js
+++ src/main/webapp/resources/js/global.js
@@ -3,14 +3,45 @@ $(document).ready(function () {
         e.preventDefault();
         $(".logout-form").submit();
     });
+    $("#orderOfSort").click(function (e) {
+        e.preventDefault();
+        if($("#sortIcon").hasClass('glyphicon glyphicon-sort-by-attributes-alt')){
+            $("#sortIcon").removeClass('glyphicon-sort-by-attributes-alt');
+            $("#sortIcon").addClass('glyphicon-sort-by-attributes');
+        }
+        else{
+            $("#sortIcon").removeClass('glyphicon-sort-by-attributes');
+            $("#sortIcon").addClass('glyphicon-sort-by-attributes-alt');
+        }
+    });
+
     $.ajax({
-            url: ctx+"/order/cart/count",
+            url: ctx + "/order/cart/count",
             dataType: 'json',
-            type:'get',
-            contentType:'application/json',
-            processData:false
+            type: 'get',
+            contentType: 'application/json',
+            processData: false
         }
     )
-        .done(function (result) { $("#basket").html(result); })
-        .fail(function () { alert("the product wasn't removed from your cart"); });
-    });
+        .done(function (result) {
+            $("#basket").html(result);
+        })
+        .fail(function () {
+            alert("the product wasn't removed from your cart");
+        });
+    $.ajax({
+            url: ctx + "/order/cart/total",
+            dataType: 'json',
+            type: 'get',
+            contentType: 'application/json',
+            processData: false
+        }
+    )
+        .done(function (result) {
+            $("#total").html(result);
+        })
+        .fail(function () {
+            alert("total wasn't estimated");
+        });
+
+});
