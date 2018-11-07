b12dc74d3b21fd91fe4ded6c68289e793f50a4e3
leriil <leriil@mail.ru>
2018-11-07 18:20:29 +0300

    find all products method has the alphabetical order now

--- src/main/java/com/tsystems/tshop/repositories/ProductRepository.java
+++ src/main/java/com/tsystems/tshop/repositories/ProductRepository.java
@@ -4,19 +4,11 @@ import com.tsystems.tshop.domain.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProductRepository extends JpaRepository<Product,Long>,ProductRepositoryCustom {
-
-//    @Query(
-//            "select new com.tsystems.tshop.domain.ProductTop (p.productId, count(s.products.productId)) " +
-//                    "from Product p join p.sales s  where p.productId = s.products.productId group by p.productId")
-
-
-
-//    @Query(value = "select * from product",
-//            nativeQuery = true)
-//
-//    List<Product> getTop();
+    List<Product>findAllByOrderByNameAsc();
 
 
 }
