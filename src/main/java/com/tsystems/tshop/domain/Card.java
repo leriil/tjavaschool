2192fdbadf92cb47677617e1ecbdb57e1a04181f
leriil <leriil@mail.ru>
2018-11-07 18:07:41 +0300

    new class for paying with credit card

---
+++ src/main/java/com/tsystems/tshop/domain/Card.java
@@ -0,0 +1,37 @@
+package com.tsystems.tshop.domain;
+
+public class Card {
+
+
+    private Long cardNumber;
+
+    private String cvv;
+
+    private String userName;
+
+
+
+    public Long getCardNumber() {
+        return cardNumber;
+    }
+
+    public void setCardNumber(Long cardNumber) {
+        this.cardNumber = cardNumber;
+    }
+
+    public String getCvv() {
+        return cvv;
+    }
+
+    public void setCvv(String cvv) {
+        this.cvv = cvv;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+}
