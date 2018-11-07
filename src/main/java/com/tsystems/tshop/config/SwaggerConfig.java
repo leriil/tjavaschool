8ce21eaf497a56b29e861057c33ad998b3eded0e
leriil <leriil@mail.ru>
2018-11-07 13:42:37 +0300

    swagger enabled, docket bean added. Bean, resources -- dispatcher servlet

---
+++ src/main/java/com/tsystems/tshop/config/SwaggerConfig.java
@@ -0,0 +1,28 @@
+package com.tsystems.tshop.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import static springfox.documentation.builders.PathSelectors.any;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api(){
+        return new Docket(DocumentationType.SWAGGER_2).groupName("Tshop").select()
+                .apis(RequestHandlerSelectors.basePackage("com.tsystems.tshop.controllers"))
+                .paths(any()).build().apiInfo(new ApiInfo("Tshop",
+                        "Online shopping", "1.0.0", null,
+                        new Contact("Katya Likhacheva", "https://vk.com/leriilleriil", "leriil@mail.ru"),null, null));
+    }
+
+
+}
