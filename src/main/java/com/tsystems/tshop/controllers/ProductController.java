0c45696dae3a560bf7b61e4c2c0f7811f02c2e3e
leriil <leriil@mail.ru>
2018-11-07 21:17:52 +0300

    minor changes

--- src/main/java/com/tsystems/tshop/controllers/ProductController.java
+++ src/main/java/com/tsystems/tshop/controllers/ProductController.java
@@ -1,13 +1,5 @@
-package com.tsystems.tshop.controllers;//package com.tsystems.tshop.controllers;
-//
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.PathVariable;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//import com.tsystems.tshop.domain.Product;
-//import com.tsystems.tshop.services.ProductService;
-//
+package com.tsystems.tshop.controllers;
+
 
 import com.tsystems.tshop.domain.Product;
 import com.tsystems.tshop.domain.ProductTop;
