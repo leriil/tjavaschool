b8a06bed1c5a806f6d49943f00e8cfdc41fbc275
leriil <leriil@mail.ru>
2018-11-15 00:55:52 +0300

    payWithCard() sends CardWithdrawal object, instead of a number of Strings. createNewOrder() checks if the order is brand-new, sets orderId to null in case it's not.

--- src/main/java/com/tsystems/tshop/services/OrderService.java
+++ src/main/java/com/tsystems/tshop/services/OrderService.java
@@ -1,13 +1,13 @@
 package com.tsystems.tshop.services;
 
 import com.tsystems.tshop.domain.*;
+import com.tsystems.tshop.enums.OrderStatus;
+import com.tsystems.tshop.enums.PaymentStatus;
 import com.tsystems.tshop.repositories.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
 import javax.transaction.Transactional;
@@ -15,10 +15,14 @@ import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 import java.util.logging.Logger;
+
+/**
+ *
+ */
 @Service
 public class OrderService {
 
-    private static final java.util.logging.Logger log = Logger.getLogger("LOGGER");
+    private static final java.util.logging.Logger log = Logger.getLogger(OrderService.class.toString());
 
     OrderRepository orderRepository;
 
@@ -52,32 +56,57 @@ public class OrderService {
         log.info( "we have saved::" + order.toString());
     }
 
-   public void payWithCard(Card card,Order order,List<Product>products){
 
-       HttpHeaders headers=new HttpHeaders();
-       headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+    /**
+     *
+     * @param card
+     * @param order
+     * @param products
+     */
+   public void payWithCard(Card card,Order order,List<Product>products) throws RuntimeException{
 
-       MultiValueMap<String,String> map = new LinkedMultiValueMap<>();
 
-       map.set("number", String.valueOf(card.getCardNumber()));
-       map.set("cvv", String.valueOf(card.getCvv()));
-       map.set("userName",String.valueOf(card.getUserName()));
-       map.set("total",String.valueOf(this.productService.getTotal(products)));
+//       HttpMessageConverter messageConverter = new MappingJackson2HttpMessageConverter();
+//       List<HttpMessageConverter<?>> converters=new ArrayList<>();
+//       converters.add(messageConverter);
+//       restTemplate.setMessageConverters((converters));
 
-       HttpEntity<MultiValueMap<String,String>> request=new HttpEntity<>(map,headers);
+//       MultiValueMap<String,Object> map=new LinkedMultiValueMap<>();
+//
+//       map.add("card", card);
+//       map.add("total",this.productService.getTotal(products));
+//
+//       HttpEntity<MultiValueMap<String,Object>> request=new HttpEntity<>(map,headers);
 
-       ResponseEntity<String> response=restTemplate.postForEntity(paymentServiceUrl+cards+payment,request,String.class);
+//       HttpHeaders headers=new HttpHeaders();
+//       headers.setContentType(MediaType.APPLICATION_JSON);
+
+//       HttpEntity<CardWithdrawal> request=new HttpEntity<>(data,headers);
+      // map.set("number", String.valueOf(card.getCardNumber()));
+//       map.set("cvv", String.valueOf(card.getCvv()));
+//       map.set("userName",String.valueOf(card.getUserName()));
+//       map.set("total",String.valueOf(this.productService.getTotal(products)));
+       //CardData cd = ...;
+       // use multipartBody with RestTemplate or WebClient
+//       HttpEntity<MultiValueMap<String, Object>> request
+//               = new HttpEntity<>(map, headers);
+
+       CardWithdrawal data=new CardWithdrawal(card,productService.getTotal(products));
+       HttpEntity<CardWithdrawal> request=new HttpEntity<>(data);
+       ResponseEntity<String> response=restTemplate
+               .postForEntity(paymentServiceUrl+cards+payment,request,String.class);
+
 
        if(response.getStatusCode().equals(HttpStatus.OK)){
-           order.setPaymentStatus("paid");
+           order.setPaymentStatus(PaymentStatus.PAID);
            this.saveOrder(order,products);
-       }
+       }else throw new RuntimeException();
    }
 //TODO: change the inStock value before saving the order
     public void saveOrder(Order order, List<Product> products) {
         log.info("Transaction \"saveOrder\" should have started here");
         this.addressService.save(order.getAddress());
-        order.getUser().getAddresses().add(order.getAddress());
+        order.getUser().setAddress(order.getAddress());
         this.userService.saveUser(order.getUser());
         order.setProducts(products);
         this.orderRepository.save(order);
@@ -107,14 +136,21 @@ public class OrderService {
 //TODO: figure out if the return value is needed
     public Order createNewOrder(Order order) {
 
-        User user = this.userService.getUser();
-        order.setUser(user);
-        Address currentAddress = this.addressService.userCurrentAddress(user);
-        if (currentAddress != null) {
-            order.setAddress(currentAddress);
+        if(order.getOrderId()!=null){
+            order.setOrderId(null);
         }
-        order.setOrderStatus("waiting for approval");
-        order.setPaymentStatus("not paid");
+
+        else {
+            User user = this.userService.getUser();
+            order.setUser(user);
+            Address currentAddress = user.getAddress();
+            if (currentAddress != null) {
+                order.setAddress(currentAddress);
+            }
+        }
+
+        order.setOrderStatus(OrderStatus.PENDING_APPROVAL);
+        order.setPaymentStatus(PaymentStatus.NOT_PAID);
         log.info("after creating a new order: " +order);
         return order;
     }
