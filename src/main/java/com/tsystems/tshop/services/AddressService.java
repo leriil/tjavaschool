08a078aa7a7b9bdb1714b64ced9422615ed097da
leriil <leriil@mail.ru>
2018-11-15 00:53:13 +0300

    removed userCurrentAddress()

--- src/main/java/com/tsystems/tshop/services/AddressService.java
+++ src/main/java/com/tsystems/tshop/services/AddressService.java
@@ -1,11 +1,9 @@
 package com.tsystems.tshop.services;
 
 import com.tsystems.tshop.domain.Address;
-import com.tsystems.tshop.domain.User;
 import com.tsystems.tshop.repositories.AddressRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.logging.Logger;
 
@@ -21,12 +19,12 @@ public class AddressService {
         this.addressRepository = addressRepository;
     }
 
-    @Transactional
-    public Address userCurrentAddress(User user){
-    if(user.getAddresses().iterator().hasNext()){
-        return user.getAddresses().iterator().next();
-    }else return null;
-}
+//    @Transactional
+//    public Address userCurrentAddress(User user){
+//    if(user.getAddresses().iterator().hasNext()){
+//        return user.getAddresses().iterator().next();
+//    }else return null;
+//}
 
     public void save(Address address){
         this.addressRepository.save(address);
