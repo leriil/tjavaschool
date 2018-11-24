67ce7e67267ecb75b94655e0f41cc4f94c012f70
leriil <leriil@mail.ru>
2018-11-24 03:49:51 +0300

    adding a new category to the Product

--- src/main/webapp/resources/js/addProduct.js
+++ src/main/webapp/resources/js/addProduct.js
@@ -1,4 +1,39 @@
 $(function() {
+    $.get(
+        ctx+"/product/categories",
+        function (result) {
+           addOptions(result);
+           addDeviderAndUserOption();
+           // addCategoryToProduct();
+            }
+    );
+    addCategoryToProduct();
+
+
+});
+function addCategoryToProduct() {
+    $('#addProductForReview').click(function () {
+            var csrfParameter = $("meta[name='_csrf_parameter']").attr("content");
+            var csrfHeader = $("meta[name='_csrf_header']").attr("content");
+            var csrfToken = $("meta[name='_csrf']").attr("content");
+            var headers = {};
+            headers[csrfHeader] = csrfToken;
+            var value=$(":selected").val();
+            var text=$(":selected").text();
+            $.ajax({
+                    url: ctx + "/product/category/add",
+                    dataType: 'json',
+                    type: 'post',
+                    headers: headers,
+                    contentType: 'text/plain',
+                    data: value
+                    // processData: false
+                }
+            );
+        }
+    );
+}
+function addDeviderAndUserOption(){
     var content = "<input type='text' class='bss-input' onKeyDown='event.stopPropagation();' onKeyPress='addSelectInpKeyPress(this,event)' onClick='event.stopPropagation()' placeholder='Add item'> <span class='glyphicon glyphicon-plus addnewicon' onClick='addSelectItem(this,event,1);'></span>";
 
     var divider = $('<option/>')
@@ -12,24 +47,34 @@ $(function() {
     $('.selectpicker')
         .append(divider)
         .append(addoption)
-        .selectpicker();
+        .selectpicker('refresh');
 
-});
+}
+function addOptions(options) {
+    for (var i = 0; i < options.length; i++) {
+        var option = $('<option/>').text(options[i].categoryName);
+        $('.selectpicker')
+            .append(option);
+    }
+    $('.selectpicker').selectpicker('refresh');
+}
 
 function addSelectItem(t,ev)
 {
     ev.stopPropagation();
 
     var bs = $(t).closest('.bootstrap-select')
-    var txt=bs.find('.bss-input').val().replace(/[|]/g,"");
-    var txt=$(t).prev().val().replace(/[|]/g,"");
+    // var txt=bs.find('.bss-input').val().replace(/[|]/g,"").toUpperCase();
+    var txt=$(t).prev().val().replace(/[|]/g,"").toUpperCase();
     if ($.trim(txt)=='') return;
 
     // Changed from previous version to cater to new
     // layout used by bootstrap-select.
     var p=bs.find('select');
     var o=$('option', p).eq(-2);
-    o.before( $("<option>", { "selected": true, "text": txt}) );
+    o.before( $("<option>", {
+        "selected": true,
+        "text": txt}) );
     p.selectpicker('refresh');
 }
 
