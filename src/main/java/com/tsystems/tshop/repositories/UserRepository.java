c3f71c87a2c5e89c98c4decc4aaef6dab7153521
leriil <leriil@mail.ru>
2018-11-24 03:44:44 +0300

    Custom interface added

--- src/main/java/com/tsystems/tshop/repositories/UserRepository.java
+++ src/main/java/com/tsystems/tshop/repositories/UserRepository.java
@@ -3,7 +3,7 @@ package com.tsystems.tshop.repositories;
 import com.tsystems.tshop.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User,Long> {
+public interface UserRepository extends JpaRepository<User,Long>, UserRepositoryCustom {
     User findByLogin(String login);
 
 
