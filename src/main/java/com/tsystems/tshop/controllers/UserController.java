782ae7bf804d5ed9892eda2e6b2266914331962b
leriil <leriil@mail.ru>
2018-11-24 03:39:50 +0300

    @Valid, /find/top, /{userId}, @InitBinder

--- src/main/java/com/tsystems/tshop/controllers/UserController.java
+++ src/main/java/com/tsystems/tshop/controllers/UserController.java
@@ -1,16 +1,20 @@
 package com.tsystems.tshop.controllers;
 
 import com.tsystems.tshop.domain.User;
+import com.tsystems.tshop.domain.UserTop;
 import com.tsystems.tshop.services.UserService;
+import com.tsystems.tshop.validators.PasswordValidator;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @Controller
 @RequestMapping("/user")
@@ -42,9 +46,34 @@ public class UserController {
     }
 
     @RequestMapping(value = "/account/update", method = RequestMethod.POST)
-    public String updateAccount(@ModelAttribute("user") User user){
+    public String updateAccount(@Valid @ModelAttribute("user") User user,
+                                Errors errors){
+        if(!errors.hasErrors()){
         this.userService.updateUserInfo(user);
-        return "redirect:/product/all";
+        return "redirect:/product/all";}
+        return "account";
     }
 
+    @ResponseBody
+    @RequestMapping("/find/top")
+    public List<UserTop> getTopUsers(
+            @RequestParam(value = "order", required = false) String order
+    ){
+        return userService.getTopUsers(order);
+    }
+
+    @RequestMapping(value = "/{userId}")
+    public String findProduct(Model model,
+                              @PathVariable Long userId) {
+        try {
+            model.addAttribute("user", userService.getUserById(userId));
+        } catch (RuntimeException e) {
+            return "user_not_found";
+        }
+        return "user";
+    }
+    @InitBinder
+    public void initBinder(WebDataBinder binder){
+binder.addValidators(new PasswordValidator());
+    }
 }
