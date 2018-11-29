ab36a1a62e4072d8e13c2522e8031de71173f856
leriil <leriil@mail.ru>
2018-11-29 23:52:10 +0300

    @GetMapping

--- src/main/java/com/tsystems/tshop/controllers/LoginController.java
+++ src/main/java/com/tsystems/tshop/controllers/LoginController.java
@@ -2,14 +2,14 @@ package com.tsystems.tshop.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("/login")
 public class LoginController {
 
-    @RequestMapping(method = RequestMethod.GET)
+    @GetMapping()
     public String loginPage(Model model){
         return "login";
     }
