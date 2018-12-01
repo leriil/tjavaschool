4f6c205670743aee83cd42aced9fe47bada96b90
leriil <leriil@mail.ru>
2018-12-01 23:22:31 +0300

    added OrderRepositoryCustom and a method for getting orders for a period of time

--- src/main/java/com/tsystems/tshop/repositories/OrderRepository.java
+++ src/main/java/com/tsystems/tshop/repositories/OrderRepository.java
@@ -4,12 +4,16 @@ import com.tsystems.tshop.domain.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.List;
 import java.util.Set;
 
 @Repository
-public interface OrderRepository extends JpaRepository<Order,Long> {
+public interface OrderRepository extends JpaRepository<Order, Long>, OrderRepositoryCustom {
 
     Set<Order>findAllByUser_Login(String login);
+
+    List<Order> findAllByOrderDateBetween(LocalDate first, LocalDate last);
 
 
 }
