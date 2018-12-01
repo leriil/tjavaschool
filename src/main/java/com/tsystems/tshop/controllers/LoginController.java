55269fbc9c608843c9b09f9701ebd4c7d689dc90
leriil <leriil@mail.ru>
2018-12-01 18:34:42 +0300

    removed Model from loginPage()

--- src/main/java/com/tsystems/tshop/controllers/LoginController.java
+++ src/main/java/com/tsystems/tshop/controllers/LoginController.java
@@ -1,7 +1,6 @@
 package com.tsystems.tshop.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -10,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class LoginController {
 
     @GetMapping()
-    public String loginPage(Model model){
+    public String loginPage() {
         return "login";
     }
 
