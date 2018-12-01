839a764f4ee97b1581278863c5e15cf2fa075d53
leriil <leriil@mail.ru>
2018-12-01 23:19:29 +0300

    moved to repositories/impl

--- src/main/java/com/tsystems/tshop/repositories/impl/ProductRepositoryImpl.java
+++ src/main/java/com/tsystems/tshop/repositories/impl/ProductRepositoryImpl.java
@@ -1,6 +1,7 @@
-package com.tsystems.tshop.repositories;
+package com.tsystems.tshop.repositories.impl;
 
 import com.tsystems.tshop.domain.ProductTop;
+import com.tsystems.tshop.repositories.ProductRepositoryCustom;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -8,27 +9,24 @@ import java.util.List;
 
 public class ProductRepositoryImpl implements ProductRepositoryCustom {
 
-    @PersistenceContext
-    EntityManager em;
-
-    private static final String topProducts="select product_id as productId, name, product.category_id as categoryId, category_name as categoryName, in_stock as inStock, price, " +
+    private static final String TOP_PRODUCTS = "select product_id as productId, name, product.category_id as categoryId, category_name as categoryName, in_stock as inStock, price, " +
             "volume, weight, top from product left join " +
             "(select product.product_id as pid, count(order_product.product_id) as top " +
             "from product join order_product " +
             "on product.product_id=order_product.product_id " +
             "group by pid) as s on product.product_id=s.pid " +
             "left join category on product.category_id=category.category_id ";
-
-    private static final String orderAsc="order by top asc";
-
-    private static final String orderDesc="order by top desc";
+    private static final String ORDER_ASC = "order by top asc";
+    private static final String ORDER_DESC = "order by top desc";
+    @PersistenceContext
+    private EntityManager em;
 
     @Override
     @SuppressWarnings("unchecked")
     public List<ProductTop> getTopProductsDesc() {
 
         return
-                em.createNativeQuery(topProducts+orderDesc,
+                em.createNativeQuery(TOP_PRODUCTS + ORDER_DESC,
                         "productTopMapping")
                         .getResultList();
 
@@ -38,7 +36,7 @@ public class ProductRepositoryImpl implements ProductRepositoryCustom {
     public List<ProductTop> getTopProductsAsc(){
 
         return
-                em.createNativeQuery(topProducts+orderAsc,
+                em.createNativeQuery(TOP_PRODUCTS + ORDER_ASC,
                         "productTopMapping")
                         .getResultList();
     }
