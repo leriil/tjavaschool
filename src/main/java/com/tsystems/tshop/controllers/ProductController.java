c153daae9627440526a1b970586d2149e9361314
leriil <leriil@mail.ru>
2018-12-01 18:52:03 +0300

    LOGGER, @GetMapping, @PostMapping, added status.setComplete() to findProduct. FindProductsByName() added @RequestParam for name, added attributes to model. HandleUpload for uploading an xml file. ShowUploadPage added.

--- src/main/java/com/tsystems/tshop/controllers/ProductController.java
+++ src/main/java/com/tsystems/tshop/controllers/ProductController.java
@@ -6,16 +6,18 @@ import com.tsystems.tshop.domain.ProductTop;
 import com.tsystems.tshop.services.CategoryService;
 import com.tsystems.tshop.services.ProductService;
 import io.swagger.annotations.Api;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.support.SessionStatus;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.Valid;
 import java.util.List;
-import java.util.logging.Logger;
 
 @Controller
 @SessionAttributes({"product"})
@@ -23,10 +25,10 @@ import java.util.logging.Logger;
 @Api(value = "product",description = "managing products",tags = "products")
 public class ProductController {
 
-    private static final Logger log = Logger.getLogger("LOGGER");
+    private static final Logger LOGGER = LogManager.getLogger(ProductController.class);
 
-    ProductService productService;
-    CategoryService categoryService;
+    private final ProductService productService;
+    private final CategoryService categoryService;
 
     @Autowired
     public ProductController(ProductService productService,
@@ -49,11 +51,6 @@ public class ProductController {
 
     }
 
-//    @ModelAttribute("categoryOptions")
-//    public List<ProductCategory> getCategories() {
-//        return new LinkedList<>(Arrays.asList(ProductCategory.values()));
-//    }
-
     @ResponseBody
     @RequestMapping("/categories")
     public List<com.tsystems.tshop.domain.ProductCategory> getCategories(){
@@ -73,9 +70,9 @@ public class ProductController {
     @RequestMapping("/review")
     public String reviewProduct(@Valid @ModelAttribute Product product,
                                 Errors errors) {
-        if(!errors.hasErrors())
-        {return "product_review";}
-
+        if (!errors.hasErrors()) {
+            return "product_review";
+        }
         return "product_add";
     }
 
@@ -96,12 +93,15 @@ public class ProductController {
         }
     }
 
-    //TODO: make a view for this method
+
     @ResponseBody
     @RequestMapping(value = "/find/top")
     public List<ProductTop> topProducts(
             @RequestParam(value = "order", required = false) String order
     ) {
+        List<ProductTop> productTopList = productService.getTopProducts(order);
+
+        LOGGER.info("productTop from controller" + productTopList);
         return productService.getTopProducts(order);
 
     }
@@ -121,20 +121,36 @@ public class ProductController {
 
     @RequestMapping(value = "/{productId}")
     public String findProduct(Model model,
-                              @PathVariable Long productId) {
-        try {
+                              @PathVariable Long productId,
+                              SessionStatus status) {
+
             model.addAttribute("product", this.productService.findProductById(productId));
-        } catch (RuntimeException e) {
-            return "product_not_found";
-        }
+        status.setComplete();
         return "product";
     }
 
-    //TODO: this method will be used by sales stuff to search for a particular product
-    @ResponseBody
-    @RequestMapping(value = "/find/{name}")
-    public List<Product> findProductsByName(@PathVariable String name){
-        return this.productService.findProductByName(name);
+    @RequestMapping(value = "/find")
+    public String findProductsByName(Model model, @RequestParam("search") String name) {
+        List<Product> productSearch = productService.findProductByName(name);
+        model.addAttribute("itemsNotFound", productSearch.isEmpty());
+        model.addAttribute("productSearch", productSearch);
+        return "products_search";
+    }
+
+    @RequestMapping(value = "/upload", method = RequestMethod.POST)
+    public String handleUpload(@RequestParam("product_file") MultipartFile file) {
+        try {
+            productService.parseFile(file);
+            return "redirect:/product/file/upload?fileUploaded=true";
+        } catch (Exception e) {
+            return "redirect:/product/file/upload?fileUploaded=false";
+        }
+    }
+
+    @GetMapping("/file/upload")
+    public String showUploadPage(
+    ) {
+        return "file_upload";
     }
 
     @RequestMapping("/stats")
