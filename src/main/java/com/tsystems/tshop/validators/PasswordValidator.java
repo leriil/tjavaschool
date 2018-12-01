9af8c971b8bc5bdb07481b78fea7ee90aa7c4422
leriil <leriil@mail.ru>
2018-12-02 02:57:20 +0300

    removed unnecessary comments, changed field password to confirmPassword

--- src/main/java/com/tsystems/tshop/validators/PasswordValidator.java
+++ src/main/java/com/tsystems/tshop/validators/PasswordValidator.java
@@ -2,7 +2,6 @@ package com.tsystems.tshop.validators;
 
 import com.tsystems.tshop.domain.User;
 import org.springframework.validation.Errors;
-import org.springframework.validation.ValidationUtils;
 import org.springframework.validation.Validator;
 
 public class PasswordValidator implements Validator {
@@ -13,26 +12,20 @@ public class PasswordValidator implements Validator {
 
     @Override
     public void validate(Object target, Errors errors) {
-
-        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "password","user.password","this field should not be empty or contain whitespaces");
-        ValidationUtils.rejectIfEmptyOrWhitespace(errors,"confirmPassword","user.confirm.password", "this field should not be empty or contain whitespaces");
-//TODO: add a regex for the password
-//        ^                 # start-of-string
 //                (?=.*[0-9])       # a digit must occur at least once
 //                (?=.*[a-z])       # a lower case letter must occur at least once
 //                (?=.*[A-Z])       # an upper case letter must occur at least once
-//                (?=.*[@#$%^&+=])  # a special character must occur at least once
-//        (?=\S+$)          # no whitespace allowed in the entire string
+//                (?=\S+$)          # no whitespace allowed in the entire string
 //                .{8,}             # anything, at least eight places though
 //        $                 # end-of-string
-        String pattern = "^(?=.*[0-9])(?=.*[0-9])(?=.*[A-Z]).{8,}$";
-//TODO: Patternm Matcher here
+        String pattern = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{4,}$";
+
         User user =(User) target;
         if(!user.getPassword().matches(pattern)){
-            errors.rejectValue("password","user.password","the password contains illegal characters or is too weak");
+            errors.rejectValue("password", "user.password", "the password is too weak");
         }
         if(!user.getPassword().equals(user.getConfirmPassword())){
-            errors.rejectValue("password","user.password", "password confirmation failed");
+            errors.rejectValue("confirmPassword", "user.confirm.password", "password confirmation failed");
         }
     }
 }
