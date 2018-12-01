ca08e3a9bb8193184440a293e9c9e2cf3040a04e
leriil <leriil@mail.ru>
2018-12-01 23:38:15 +0300

    LOGGER, private final

--- src/main/java/com/tsystems/tshop/services/AddressService.java
+++ src/main/java/com/tsystems/tshop/services/AddressService.java
@@ -2,38 +2,27 @@ package com.tsystems.tshop.services;
 
 import com.tsystems.tshop.domain.Address;
 import com.tsystems.tshop.repositories.AddressRepository;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.logging.Logger;
 
 @Service
 public class AddressService {
 
-    private static final Logger log = Logger.getLogger("LOGGER");
+    private static final Logger LOGGER = LogManager.getLogger(AddressService.class);
 
-    AddressRepository addressRepository;
+    private final AddressRepository addressRepository;
 
     @Autowired
     public AddressService(AddressRepository addressRepository) {
         this.addressRepository = addressRepository;
     }
 
-//    @Transactional
-//    public Address userCurrentAddress(User user){
-//    if(user.getAddresses().iterator().hasNext()){
-//        return user.getAddresses().iterator().next();
-//    }else return null;
-//}
-
     public void save(Address address){
         this.addressRepository.save(address);
     }
 
-
-    public void saveAddress(Address oldAddress, Address newAddress) {
-
-    }
 }
 
 
