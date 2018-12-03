11defcad2cb467cd504c511e71a3a620afaf1986
leriil <leriil@mail.ru>
2018-12-03 05:43:49 +0300

    code style changes

--- src/main/java/com/tsystems/tshop/controllers/ProductController.java
+++ src/main/java/com/tsystems/tshop/controllers/ProductController.java
@@ -18,11 +18,12 @@ import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.Valid;
 import java.util.List;
+import java.util.Objects;
 
 @Controller
 @SessionAttributes({"product"})
 @RequestMapping("/product")
-@Api(value = "product", description = "managing products", tags = "products")
+@Api(value = "product", tags = "products")
 public class ProductController {
 
     private static final Logger LOGGER = LogManager.getLogger(ProductController.class);
@@ -33,6 +34,7 @@ public class ProductController {
     @Autowired
     public ProductController(ProductService productService,
                              CategoryService categoryService) {
+
         this.productService = productService;
         this.categoryService = categoryService;
     }
@@ -44,9 +46,10 @@ public class ProductController {
     }
 
     @ResponseBody
-    @RequestMapping(value = "/category/add", method = RequestMethod.POST)
+    @PostMapping("/category/add")
     public void setProductCategory(@RequestBody String categoryName,
                                    @ModelAttribute Product product) {
+
         product.setCategory(categoryService.getCategoryByName(categoryName));
 
     }
@@ -54,6 +57,7 @@ public class ProductController {
     @ResponseBody
     @RequestMapping("/categories")
     public List<com.tsystems.tshop.domain.ProductCategory> getCategories() {
+
         return categoryService.getCategories();
     }
 
@@ -63,26 +67,29 @@ public class ProductController {
             @RequestParam(value = "column", required = false) String column,
             @RequestParam(value = "order", required = false) String order
     ) {
+
         return this.productService.sortProducts(column, order);
     }
 
     @RequestMapping("/review")
     public String reviewProduct(@Valid @ModelAttribute Product product,
                                 Errors errors) {
+
         if (!errors.hasErrors()) {
             return "product_review";
         }
         return "product_add";
     }
 
-    @RequestMapping(value = "/add", method = RequestMethod.GET)
-    public String addProduct(Model model,
-                             @ModelAttribute("product") Product product) {
+    @GetMapping(value = "/add")
+    public String addProduct(@ModelAttribute("product") Product product) {
+
         return "product_add";
     }
 
     @RequestMapping(value = "/save")
     public String saveProduct(@ModelAttribute Product product, SessionStatus status) {
+
         try {
             this.productService.save(product);
             status.setComplete();
@@ -92,30 +99,30 @@ public class ProductController {
         }
     }
 
-
     @ResponseBody
     @RequestMapping(value = "/find/top")
     public List<ProductTop> topProducts(
             @RequestParam(value = "order", required = false) String order
     ) {
+
         List<ProductTop> productTopList = productService.getTopProducts(order);
 
         LOGGER.info("productTop from controller" + productTopList);
         return productService.getTopProducts(order);
-
     }
 
-    //TODO: maybe return the List of sorted products in a separate method?
     @RequestMapping("/all")
     public String findAll(Model model,
                           @RequestParam(value = "sortingOption", required = false) String sortingOption,
                           @RequestParam(value = "sortingOrder", required = false) String sortingOrder) {
-        if ((sortingOption != null) && (sortingOrder != null)) {
-//
-            model.addAttribute("products", this.productService.getSortedProducts(sortingOption, sortingOrder));
+
+        List<Product> products;
+        if ((Objects.nonNull(sortingOption)) && (Objects.nonNull(sortingOrder))) {
+            products = productService.getSortedProducts(sortingOption, sortingOrder);
         } else {
-            model.addAttribute("products", this.productService.findProducts());
+            products = productService.findProducts();
         }
+        model.addAttribute("products", products);
         return "products";
     }
 
@@ -131,14 +138,16 @@ public class ProductController {
 
     @RequestMapping(value = "/find")
     public String findProductsByName(Model model, @RequestParam("search") String name) {
+
         List<Product> productSearch = productService.findProductByName(name);
         model.addAttribute("itemsNotFound", productSearch.isEmpty());
         model.addAttribute("productSearch", productSearch);
         return "products_search";
     }
 
-    @RequestMapping(value = "/upload", method = RequestMethod.POST)
+    @PostMapping("/upload")
     public String handleUpload(@RequestParam("product_file") MultipartFile file) {
+
         try {
             productService.parseFile(file);
             return "redirect:/product/file/upload?fileUploaded=true";
@@ -150,17 +159,14 @@ public class ProductController {
     @GetMapping("/file/upload")
     public String showUploadPage(
     ) {
+
         return "file_upload";
     }
 
     @RequestMapping("/stats")
     public String getStats() {
+
         return "statistics";
     }
-
-//    @ExceptionHandler(NullPointerException.class)
-//    public String handleError(){
-//        return "controller_error";
-//    }
 }
 
